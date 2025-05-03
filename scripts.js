b8a2b6413dd0639fc463631af80fd24348412882
Brett Alford <brettalford1@gmail.com>
2025-05-03 02:50:39 -0500

    finished linking buttons and making it all work

--- scripts.js
+++ scripts.js
@@ -71,12 +71,12 @@ function hslToRgb(h, s, l) {
 }
 
 function addHueRandomness(hue) {
-    let randomOffset = (Math.random() * 36 - 18); // ±5% of 360
+    let randomOffset = (Math.random() * 50 - 25); 
     hue = (hue + randomOffset + 360) % 360;
     return hue;
 }
 
-// Complementary color with ~5% randomness
+// Complementary color with randomness
 function getComplementaryColor(rgb) {
     let [h, s, l] = rgbToHsl(rgb);
     h = (h + 180) % 360;
@@ -146,99 +146,47 @@ function rgbToHex(rgb){
 
 //So many things need to be put in im going to go crazy
 
-let locked=[false,false,false,false,false];
-let starred=numgen();
-
-
-
-
-
-//only one can be starred, when starred the rgb of that (c1,c2,c3,c4, or c5) become the starred value and icon image is changed to its
-//counterpart so that only one can be starred at a time, this starring is used as a way to select which color the analogous, 
-//set by default to c1
-function staring(starnum){
-    switch(starnum){
-        case 1:
-        
-        break;
-        case 2:
-        
-        break;
-        case 3:
-        
-        break;
-        case 4:
-        
-        break;
-        case 5:
-        
-        break;
-        default:
-        break;
-    }
-}
-
-
-
-//locking/unlocking changes the icon and prevents the colors of those versions from being changed without being unlocked
-function locking(locknum){
-    switch(locknum){
-        case 1:
-        
-        break;
-        case 2:
-        
-        break;
-        case 3:
-        
-        break;
-        case 4:
-        
-        break;
-        case 5:
-        
-        break;
-        default:
-        break;
-    }
-}
 
 
 
 function compl(){
     let holdclash;
+    const elements=[c1,c2,c3,c4,c5]
     console.log(rgbToHex(starred));
     for(let i=0;i<=4;i++){
         if(locked[i] != true){
-            holdclash=rgbToHex(getComplementaryColor(starred));
-            console.log(holdclash);
+            holdcompl=rgbToHex(getComplementaryColor(starred));
+            elements[i].style.backgroundColor=holdcompl;
     }
     }
+    updateUI();
 
 }
 
 
 function clash(){
     let holdclash;
-    console.log(rgbToHex(starred));
+    const elements=[c1,c2,c3,c4,c5]
     for(let i=0;i<=4;i++){
         if(locked[i] != true){
             holdclash=rgbToHex(getClashingColor(starred));
-            console.log(holdclash);
+            elements[i].style.backgroundColor=holdclash;
     }
     }
+    updateUI();
 }
 
 
 function analog(){
     let holdana;
-    console.log(rgbToHex(starred));
+    const elements=[c1,c2,c3,c4,c5]
     for(let i=0;i<=4;i++){
         if(locked[i] != true){
             holdana=rgbToHex(getAnalogousColor(starred));
-            console.log(holdana);
+            elements[i].style.backgroundColor=holdana;
     }
     }
+    updateUI();
 }
 
 function randy(){
@@ -248,14 +196,16 @@ function randy(){
         if(locked[i] != true){
             holdrand=rgbToHex(numgen());
             elements[i].style.backgroundColor=holdrand;
-            console.log(`Assigned to c${i + 1}: ${holdrand}`);
     }
     }
+    updateUI();
 }
 
 
 
 
+
+//lower button section
 
 const c1=document.getElementById("c1");
 const c2=document.getElementById("c2");
@@ -267,8 +217,158 @@ const c5=document.getElementById("c5");
 
 const random=document.getElementById("random");
 random.onclick=randy;
+const analo=document.getElementById("analo");
+analo.onclick=analog;
+const clashing=document.getElementById("clashing");
+clashing.onclick=clash;
+const complementary=document.getElementById("complementary");
+complementary.onclick=compl;
 
 
+
+
+//lock buttons and function
+
+const icon=[
+    document.getElementById("lockimg1"),
+    document.getElementById("lockimg2"),
+    document.getElementById("lockimg3"),
+    document.getElementById("lockimg4"),
+    document.getElementById("lockimg5")
+];
+let locked=[false,false,false,false,false];
+function locking(locknum){
+    const elements=[c1,c2,c3,c4,c5]
+    if (locked[locknum-1]==true){
+        locked[locknum-1]=false;
+        icon[locknum-1].src = "https://github.com/brettalford/Color-Palette-Generator/blob/main/unlock_locked.png?raw=true";
+    }
+    else{
+        locked[locknum-1]=true;
+        icon[locknum-1].src = "https://github.com/brettalford/Color-Palette-Generator/blob/main/lock_locked.png?raw=true";
+    }
+
+}
+
+
+
+const lockbutton1=document.getElementById("lockbutton1");
+lockbutton1.onclick=()=>locking(1);
+const lockbutton2=document.getElementById("lockbutton2");
+lockbutton2.onclick=()=>locking(2);
+const lockbutton3=document.getElementById("lockbutton3");
+lockbutton3.onclick=()=>locking(3);
+const lockbutton4=document.getElementById("lockbutton4");
+lockbutton4.onclick=()=>locking(4);
+const lockbutton5=document.getElementById("lockbutton5");
+lockbutton5.onclick=()=>locking(5);
+
+
+
+
+
+let starred=numgen();
+
+//only one can be starred, when starred the rgb of that (c1,c2,c3,c4, or c5) become the starred value and icon image is changed to its
+//counterpart so that only one can be starred at a time, this starring is used as a way to select which color the analogous, 
+//set by default to c1
+function parseRGBString(rgbString) {
+    let result = rgbString.match(/\d+/g);
+    return result ? result.slice(0, 3).map(Number) : [0, 0, 0];
+}
+
+function staring(starnum){
+    const elements=[c1,c2,c3,c4,c5];
+    const icons = [
+        document.getElementById("starimg1"),
+        document.getElementById("starimg2"),
+        document.getElementById("starimg3"),
+        document.getElementById("starimg4"),
+        document.getElementById("starimg5")
+    ];
+    starred=parseRGBString(elements[starnum-1].style.backgroundColor);
+    for (let i = 0; i < 5; i++) {
+        if (i === starnum - 1) {
+            locked[i] = true;
+            icon[i].src = "https://github.com/brettalford/Color-Palette-Generator/blob/main/lock_locked.png?raw=true"
+            icons[i].src = "https://github.com/brettalford/Color-Palette-Generator/blob/main/star-starred.png?raw=true";
+        }
+        else{
+            icons[i].src = "https://github.com/brettalford/Color-Palette-Generator/blob/main/star.png?raw=true";
+        }
+    }
+}
+
+const starbutton1=document.getElementById("starbutton1");
+starbutton1.onclick=()=>staring(1);
+const starbutton2=document.getElementById("starbutton2");
+starbutton2.onclick=()=>staring(2);
+const starbutton3=document.getElementById("starbutton3");
+starbutton3.onclick=()=>staring(3);
+const starbutton4=document.getElementById("starbutton4");
+starbutton4.onclick=()=>staring(4);
+const starbutton5=document.getElementById("starbutton5");
+starbutton5.onclick=()=>staring(5);
+
+
+
+//hexcode boxes
+const hex1 = document.getElementById("hex1");
+const hex2 = document.getElementById("hex2");
+const hex3 = document.getElementById("hex3");
+const hex4 = document.getElementById("hex4");
+const hex5 = document.getElementById("hex5");
+
+const hexInputs = [hex1, hex2, hex3, hex4, hex5];
+const colorDivs = [c1, c2, c3, c4, c5];
+
+function isValidHex(hex) {
+    return /^#([0-9A-Fa-f]{6})$/.test(hex);
+}
+
+// Update color divs and input fields when color is generated
+function updateUI() {
+    for (let i = 0; i < 5; i++) {
+        let bg = colorDivs[i].style.backgroundColor;
+        let rgb = parseRGBString(bg);
+        hexInputs[i].value = rgbToHex(rgb);
+    }
+}
+
+// Let user change the input field manually
+hexInputs.forEach((input, index) => {
+    input.addEventListener("input", () => {
+        const hex = input.value;
+        if (isValidHex(hex)) {
+            colorDivs[index].style.backgroundColor = hex;
+
+            // If this color is currently starred, update the starred value
+            const starredStyle = colorDivs[index].style.backgroundColor;
+            if (parseRGBString(starredStyle).toString() === starred.toString()) {
+                starred = parseRGBString(starredStyle);
+            }
+        }
+    });
+});
+
+function syncColorsWithHexInputs() {
+    const elements = [c1, c2, c3, c4, c5];
+    const hexInputs = [hex1, hex2, hex3, hex4, hex5];
+
+    for (let i = 0; i < 5; i++) {
+        let bgColor = elements[i].style.backgroundColor;
+        let rgb = parseRGBString(bgColor);
+        let hex = rgbToHex(rgb);
+        hexInputs[i].value = hex;
+        elements[i].style.backgroundColor = hex; // Ensure bg is in hex
+    }
+}
+
+
+
+
+randy();
+syncColorsWithHexInputs();
 
 
 /*
